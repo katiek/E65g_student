d3062ea0019ba4e78ba5b595614e0d16e10ebb6f
katiek <kkorngold@gmail.com>
2017-03-27 12:53:58 -0400

    Assignment 3 Question 2 Commit

--- Assignment3/Assignment3/GridView.swift
+++ Assignment3/Assignment3/GridView.swift
@@ -2,14 +2,30 @@
 //  GridView.swift
 //  Assignment3
 //
-//  Created by katharinekorngold on 3/26/17.
+//  Created by katharinekorngold on 3/27/17.
 //  Copyright © 2017 Harvard Division of Continuing Education. All rights reserved.
 //
 
 import UIKit
 
 @IBDesignable class GridView: UIView {
-
+    
+    @IBInspectable var grid = Grid(0,0)
+    
+    @IBInspectable var size: Int = 20{
+        didSet{
+            self.grid = Grid(self.size,self.size)
+        }
+    }
+        
+    @IBInspectable var livingColor : UIColor = .green
+    @IBInspectable var emptyColor : UIColor = .darkGray
+    @IBInspectable var bornColor : UIColor = .cyan
+    @IBInspectable var diedColor : UIColor = .lightGray
+    @IBInspectable var gridColor : UIColor = .black
+    
+    @IBInspectable var gridWidth : CGFloat = 0
+ 
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
